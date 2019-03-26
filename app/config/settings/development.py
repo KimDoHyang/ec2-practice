5710ecc119864793b0dd4c5e3654c2632a3ae00e
DoHyang Kim <hanoul1124@gmail.com>
2019-03-26 15:01:40 +0900

    RDS DB 분리, production-development 사용 DB 분리 설정

--- app/config/settings/development.py
+++ app/config/settings/development.py
@@ -8,5 +8,5 @@ secrets = json.load(open(os.path.join(SECRETS_DIR, 'development.json')))
 
 # .secrets의  development.json을 사용
 # SECRET_KEY는 이미 base.py에서 base.json으로 import 했으므로,
-# development.json에는 DATABASE 설정만 기록
+# development.json에는 DATABASE : ec2_deploy_dev 설정만 기록
 DATABASES = secrets['DATABASES']
--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -4,9 +4,9 @@ DEBUG = False
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'production.json')))
+
+# .secrets의 production.json을 사용
+# SECRET_KEY는 이미 base.py에서 base.json으로 import 했으므로,
+# production.json에는 DATABASE : ec2_deploy 설정만 기록
+DATABASES = secrets['DATABASES']
