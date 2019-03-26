aa8a0d47cef48f13ed9912281b77235d8189e263
DoHyang Kim <hanoul1124@gmail.com>
2019-03-26 14:40:47 +0900

    spell check

--- app/config/settings/development.py
+++ app/config/settings/development.py
@@ -4,7 +4,7 @@ DEBUG = True
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-secrets = json.loads(open(os.path.join(SECRETS_DIR, 'development.json')))
+secrets = json.load(open(os.path.join(SECRETS_DIR, 'development.json')))
 
 # .secrets의  development.json을 사용
 # SECRET_KEY는 이미 base.py에서 base.json으로 import 했으므로,
